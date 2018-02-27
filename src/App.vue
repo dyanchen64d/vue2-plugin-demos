93808110783b0928aa351682f9d24477b4232489
zc <zhangchen40@126.com>
2018-02-27 14:30:05 +0800

    add vue2-plugin-message

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="message-info" @click="msgInfo">Message Info</div>
+    <div class="message-warning" @click="msgWarning">Messasge Warning</div>
   </div>
 </template>
 
@@ -25,6 +13,14 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    msgInfo () {
+      this.$message('msgInfo', 'info')
+    },
+    msgWarning () {
+      this.$message('msgWarning', 'warning')
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Message from 'vue2-plugin-message'
+
+Vue.use(Message)
 
 new Vue({
   el: '#app',
